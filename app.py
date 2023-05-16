6c1b9793a86d880e3f7d7765310c72ab987bfbab
Shbak111 <126738939+Shbak111@users.noreply.github.com>
2023-05-16 12:43:59 +0900

    Update app.py
    map

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-x = st.slider('select a value')
-st.write(x, 'suqare is ', x * x)
+df = pd.DataFrame(np.random.randn(500,2) / [50,50] + [37.76, -122.4], colums=['lat','lon])
+
+st.map(df)
